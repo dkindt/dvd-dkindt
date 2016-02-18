4a0f2bcda2d3c9451f10cf059561429801f948c2
dkindt <DanielTKindt@gmail.com>
2016-02-18 12:50:42 -0700

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,17 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
   test "should get home" do
     get :home
     assert_response :success
+  	assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get signup" do
+    get :signup
+    assert_response :success
+    assert_select "title", "Signup | Ruby on Rails Tutorial Sample App"
   end
 
 end
