b8aff1a508011cffc9d43c705aa6317edaefc0bd
dkindt <DanielTKindt@gmail.com>
2016-02-18 13:55:42 -0700

    Ruby syntax, updated test files

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,13 +5,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-  	assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+  	assert_select "title", "Ruby on Rails DVD App"
   end
 
   test "should get signup" do
     get :signup
     assert_response :success
-    assert_select "title", "Signup | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Signup | Ruby on Rails DVD App"
   end
 
 end
