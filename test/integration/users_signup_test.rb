ffd0d28e8f785d6e704a420468e0208ae063634a
dkindt <DanielTKindt@gmail.com>
2016-02-18 19:18:58 -0700

    Finish log in/log out

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -15,7 +15,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.<CSS class for field with error>'
   end
 
-    test "valid signup information" do
+  test "valid signup information" do
       get signup_path
       assert_difference 'User.count', 1 do
         post_via_redirect users_path, user: { name:  "Example User",
@@ -24,6 +24,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
       end
     assert_template 'users/show'
-    assert_not flash.FILL_IN
+    assert is_logged_in?
   end
 end
