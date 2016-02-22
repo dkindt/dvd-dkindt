54da9ae4aa48e291413fcfb6c8629504d79c0044
dkindt <DanielTKindt@gmail.com>
2016-02-22 00:48:46 -0700

    Add password resets & email configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,11 +13,19 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
-  host = 'localhost:3000'                     # Local server
+  config.action_mailer.delivery_method = :smtp
+  host = '<your heroku app>.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root 					'static_pages#home'
   get 'signup'		=>	'users#new'
   get 'login'		=>	'sessions#new'
   post 'login'		=>	'sessions#create'
   delete 'logout'	=>	'sessions#destroy'
   resources :users
-  resources :account_activations, only: [:edit]
+  resources :account_activations,	only: [:edit]
+  resources :password_resets,		only: [:new, :create, :edit, :update]
 end
