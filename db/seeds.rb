94242d84125ae719b0154aee4e30e1c8de755187
dkindt <DanielTKindt@gmail.com>
2016-02-26 21:46:22 -0700

    new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,3 +24,31 @@ User.create!(name:  "Kresten McGrath",
                activated: true,
                activated_at: Time.zone.now)
 end
+
+Movie.create!(title:  "Hot Rod",
+              release_year:  "2007",
+              available: true,
+              imdb_id:  "tt0787475",
+              poster_url:  "MV5BNjYwNjA3OTY0Nl5BMl5BanBnXkFtZTcwNDMyNDUzMw@@._V1_SX300.jpg",
+              rented_by: nil)
+
+Movie.create!(title:  "Warrior",
+              release_year:  "2011",
+              available: true,
+              imdb_id:  "tt1291584",
+              poster_url:  "MV5BMTk4ODk5MTMyNV5BMl5BanBnXkFtZTcwMDMyNTg0Ng@@._V1_SX300.jpg",
+              rented_by: nil)
+
+Movie.create!(title:  "Spectre",
+              release_year:  "2015",
+              available: true,
+              imdb_id:  "tt2379713",
+              poster_url:  "MV5BMjM2Nzg4MzkwOF5BMl5BanBnXkFtZTgwNzA0OTE3NjE@._V1_SX300.jpg",
+              rented_by: nil)
+
+Movie.create!(title:  "Bridge of Spies",
+              release_year:  "2015",
+              available: true,
+              imdb_id:  "tt3682448",
+              poster_url:  "MV5BMjIxOTI0MjU5NV5BMl5BanBnXkFtZTgwNzM4OTk4NTE@._V1_SX300.jpg",
+              rented_by: nil)
